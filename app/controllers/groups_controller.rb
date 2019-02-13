1d5d6fb84d74668642e649f7a871b9c3a216edf1
yoshiki0224 <shki0908@gmail.com>
2019-02-13 21:03:31 +0900

    グループ機能追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,16 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
   def index
   end
+
   def new
     @group = Group.new
     @group.users << current_user
   end
 
   def create
-    @group = Group.new(group_parms)
+    @group = Group.new(group_params)
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
@@ -15,8 +18,20 @@ class GroupsController < ApplicationController
     end
   end
 
+  def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
+  end
+
   private
   def group_params
-    params.require(:group).permit(:name,{ :user_ids => []})
+    params.require(:group).permit(:name, { :user_ids => [] })
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
