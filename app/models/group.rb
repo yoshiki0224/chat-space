0347beb182bb63c784369114f10288bb899d3063
yoshiki0224 <shki0908@gmail.com>
2019-02-15 18:02:27 +0900

    競合を削除

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,9 +1,6 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
-<<<<<<< HEAD
-=======
   has_many :messages
->>>>>>> 1055be327df159b918ea680e41dfa4394318512d
   validates :name, presence: true
 end
